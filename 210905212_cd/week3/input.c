74b67a26b5aa7a612bf8d1be4af06a36fdfc87e6
aashish <96538664+aashishpsv@users.noreply.github.com>
2023-08-17 16:23:53 +0530

    week3 done

---
+++ 210905212_cd/week3/input.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+	int a = 0;
+	double b = 0.0;
+	
+	char ctypee[10];
+	if(a==1){
+		return 0;
+	}
+	else 
+		return 1;
+	return 0;
+}
