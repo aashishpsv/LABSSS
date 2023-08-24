71cb4842f32f1f4022db6bbd247af8018d2b744d
aashish <96538664+aashishpsv@users.noreply.github.com>
2023-08-24 16:22:40 +0530

    week4 incomp

---
+++ 210905212_cd/week4/sample.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void printhi(int x){
+	printf("hi %d",x);
+}
+
+void print(int y){
+	printf("%d",y);
+}
+
+int main(){
+	int a,b;
+	double c;
+	a=10;
+	b=15;
+	printhi(a);
+	print(b);
+}
